6f0adafbbc055b35afb4121324646b6310d18e05
Carl Avidano <cavidano@gmail.com>
2018-12-09 13:14:24 -0500

    updated characters template

---
+++ wp-content/themes/esr_twentynineteen/page-characters.php
@@ -0,0 +1,268 @@
+<?php get_header(); ?>
+
+<main id="content">
+
+  <section class="pt-7 pb-8 bg-light torn-bottom">
+
+    <header class="container">
+      <div class="narrow text-center">
+        <h1 class="display-4"><?php the_title(); ?></h1>
+      </div>
+    </header>
+
+  </section>
+
+  <!-- Article content -->
+
+    <section class="container-fluid py-7" id="meet-the-animals">
+
+      <h2 class="text-center mb-4">Meet the Animals</h2>
+
+      <div class="wide">
+
+        <div class="row matrix-gutter">
+
+          <div class="col-lg-4">
+
+            <div class="card h-100">
+              <img class="card-img-top px-2" src="<?php echo get_template_directory_uri(); ?>/images/character-picasso-full.svg" alt="Picasso the Painted Dog">
+              <div class="card-body text-center">
+                <h3 class="card-title">Picasso</h3>
+                <h4 class="card-subtitle mb-2 font-weight-normal">The Painted Dog</h4>
+                <p class="card-text text-left">
+                  Picasso is the sneakiest of ESR’s characters! He’s so furious that African painted dogs have become
+                  endangered — he’s always a second away from smacking someone!
+                </p>
+              </div>
+              <div class="card-footer text-center">
+                <a href="#" class="btn btn-lg btn-secondary text-primary" title="Button Link">African Painted Dog Project</a>
+              </div>
+            </div>
+
+          </div>
+          <!-- .col -->
+
+          <div class="col-lg-4">
+
+            <div class="card h-100">
+              <img class="card-img-top px-2" src="<?php echo get_template_directory_uri(); ?>/images/character-cleopatra-full.svg"
+                alt="placeholder">
+              <div class="card-body text-center">
+                <h3 class="card-title">Cleopatra</h3>
+                <h4 class="card-subtitle mb-2 font-weight-normal">The Cheetah</h4>
+                <p class="card-text text-left">
+                  Speed is her middle name, so there’s no escaping Cleopatra! She may not be tricky, but her blind rage at
+                  the decline of cheetah populations has made her kooky…and dangerous!
+                </p>
+              </div>
+              <div class="card-footer text-center">
+                <a href="#" class="btn btn-lg btn-secondary text-primary" title="Button Link">Cheetah Project</a>
+              </div>
+            </div>
+
+          </div>
+          <!-- .col -->
+
+          <div class="col-lg-4">
+
+            <div class="card h-100">
+              <img class="card-img-top px-2" src="<?php echo get_template_directory_uri(); ?>/images/character-olivia-full.svg"
+                alt="placeholder">
+              <div class="card-body text-center">
+                <h3 class="card-title">Olivia</h3>
+                <h4 class="card-subtitle mb-2 font-weight-normal">From Bolivia</h4>
+                <p class="card-text text-left">
+                  This pink dolphin looks cute — but don’t be fooled by her smile! Olivia cleverly hides her anger and uses
+                  her intelligence to plot revenge against anyone who harms her friends and family.
+                </p>
+              </div>
+              <div class="card-footer text-center">
+                <a href="#" class="btn btn-lg btn-secondary text-primary" title="Button Link">Pink Dolphin Rescue</a>
+              </div>
+            </div>
+
+          </div>
+          <!-- .col -->
+
+          <div class="col-lg-4">
+
+            <div class="card h-100">
+              <img class="card-img-top px-2" src="<?php echo get_template_directory_uri(); ?>/images/character-superbear-and-panda-girl-full.svg"
+                alt="placeholder">
+              <div class="card-body text-center">
+                <h3 class="card-title">Superbear &amp; Panda Girl</h3>
+                <h4 class="card-subtitle mb-2 font-weight-normal">Lorem Ipsum Dolor</h4>
+                <p class="card-text text-left">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore neque quia officiis maxime delectus
+                  asperiores corporis
+                  veritatis, cumque tenetur consectetur.
+                </p>
+              </div>
+              <div class="card-footer text-center">
+                <a href="#" class="btn btn-lg btn-secondary text-primary" title="Button Link">Moon Bear Rescue</a>
+              </div>
+            </div>
+
+          </div>
+          <!-- .col -->
+
+          <div class="col-lg-4">
+
+            <div class="card h-100">
+              <img class="card-img-top px-2" src="<?php echo get_template_directory_uri(); ?>/images/character-antonio-full.svg"
+                alt="placeholder">
+              <div class="card-body text-center">
+                <h3 class="card-title">Antonio the Defiant</h3>
+                <h4 class="card-subtitle mb-2 font-weight-normal">The Anteater Giant</h4>
+                <p class="card-text text-left">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore neque quia officiis maxime delectus
+                  asperiores corporis
+                  veritatis, cumque tenetur consectetur.
+                </p>
+              </div>
+              <div class="card-footer text-center">
+                <!-- <a href="#" class="btn btn-lg btn-secondary text-primary" title="Button Link">Cheetah Project</a> -->
+              </div>
+            </div>
+
+          </div>
+          <!-- .col -->
+
+          <div class="col-lg-4">
+
+            <div class="card h-100">
+              <img class="card-img-top px-2" src="<?php echo get_template_directory_uri(); ?>/images/character-jed-and-amanda-full.svg"
+                alt="placeholder">
+              <div class="card-body text-center">
+                <h3 class="card-title">Jed & Amanda</h3>
+                <h4 class="card-subtitle mb-2 font-weight-normal">The Scheming Red Pandas</h4>
+                <p class="card-text text-left">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore neque quia officiis maxime delectus
+                  asperiores corporis
+                  veritatis, cumque tenetur consectetur.
+                </p>
+              </div>
+              <div class="card-footer text-center">
+                <!-- <a href="#" class="btn btn-lg btn-secondary text-primary" title="Button Link">Cheetah Project</a> -->
+              </div>
+            </div>
+
+          </div>
+          <!-- .col -->
+
+          <div class="col-lg-4">
+
+            <div class="card h-100">
+              <img class="card-img-top px-2" src="<?php echo get_template_directory_uri(); ?>/images/character-bertha-full.svg"
+                alt="placeholder">
+              <div class="card-body text-center">
+                <h3 class="card-title">Bertha</h3>
+                <h4 class="card-subtitle mb-2 font-weight-normal">The Burrowing Owl</h4>
+                <p class="card-text text-left">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore neque quia officiis maxime delectus
+                  asperiores corporis
+                  veritatis, cumque tenetur consectetur.
+                </p>
+              </div>
+            </div>
+
+          </div>
+          <!-- .col -->
+
+          <div class="col-lg-4">
+
+            <div class="card h-100">
+              <img class="card-img-top px-2" src="<?php echo get_template_directory_uri(); ?>/images/character-splash-full.svg"
+                alt="placeholder">
+              <div class="card-body text-center">
+                <h3 class="card-title">Splash</h3>
+                <h4 class="card-subtitle mb-2 font-weight-normal">The Fishing Cat</h4>
+                <p class="card-text text-left">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore neque quia officiis maxime delectus
+                  asperiores corporis
+                  veritatis, cumque tenetur consectetur.
+                </p>
+              </div>
+            </div>
+
+          </div>
+          <!-- .col -->
+
+        </div>
+        <!-- .row -->
+
+      </div>
+      <!-- .container -->
+
+    </section>
+
+    <hr>
+
+    <section class="container-fluid py-7" id="meet-the-humans">
+
+      <h2 class="text-center mb-4">Meet the Humans</h2>
+
+      <div class="wide">
+
+        <div class="row matrix-gutter">
+
+          <div class="col-lg-6">
+
+            <div class="card h-100">
+              <img class="card-img-top px-2" src="<?php echo get_template_directory_uri(); ?>/images/character-carbonita-full.svg"
+                alt="placeholder">
+              <div class="card-body text-center">
+                <h3 class="card-title">Anita</h3>
+                <h4 class="card-subtitle mb-2 font-weight-normal">aka Carbonita</h4>
+                <p class="card-text text-left">
+                  The brilliant young environmental scientist Anita, whose disastrous lab experiment to reverse climate
+                  change
+                  transforms her into the sinister, climate-change-loving Carbonita! As Carbonita, she seeks to destroy the
+                  environment – fueled by a psychotic rage against her parents, who forced her to donate her allowance to
+                  save
+                  Mexican pygmy raccoons. Each madcap episode teachesyouth 12-25 crucial ecological lessons!
+                </p>
+              </div>
+              <div class="card-footer text-center">
+                <a href="#" class="btn btn-lg btn-secondary text-primary" title="Button Link">Carbonita</a>
+              </div>
+            </div>
+
+          </div>
+          <!-- .col -->
+
+          <div class="col-lg-6">
+
+            <div class="card h-100">
+              <img class="card-img-top px-2" src="<?php echo get_template_directory_uri(); ?>/images/character-smyke-full.svg"
+                alt="placeholder">
+              <div class="card-body text-center">
+                <h3 class="card-title">Smyke</h3>
+                <h4 class="card-subtitle mb-2 font-weight-normal">Make Smyke Smart</h4>
+                <p class="card-text text-left">
+                  Smyke is not smart. But, he’s not such a bad guy. He just doesn’t know anything about animals. When he
+                  learns lessons about animals…he becomes Smart Smyke! It’s ESR’s job to “Make Smyke Smart!” <sup>TM</sup>. Each amusing episode
+                  teaches kids and their parents about a new endangered animal.
+                </p>
+              </div>
+              <div class="card-footer text-center">
+                <a href="#" class="btn btn-lg btn-secondary text-primary" title="Button Link">Make Smyke Smart</a>
+              </div>
+            </div>
+
+          </div>
+          <!-- .col -->
+
+        </div>
+        <!-- .row -->
+
+      </div>
+      <!-- .container -->
+
+    </section>
+  <!-- .container-fluid -->
+
+</main>
+<!-- #content -->
+    
+<?php get_footer(); ?>
