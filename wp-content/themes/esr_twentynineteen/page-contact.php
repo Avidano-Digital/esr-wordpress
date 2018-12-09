7e2f87c893cfd1ab5c90cf1ba211de51659eff5d
Carl Avidano <cavidano@gmail.com>
2018-12-09 14:41:28 -0500

    added contact page

---
+++ wp-content/themes/esr_twentynineteen/page-contact.php
@@ -0,0 +1,113 @@
+<?php
+/*
+Template Name: Contact
+*/
+
+get_header(); ?>
+
+<main id="content">
+
+  <section class="container-fluid bg-light torn-bottom pt-7 pb-8">
+
+    <header class="narrow text-center">
+        <h1 class="display-4">
+          <?php the_title(); ?>
+        </h1>
+    </header>
+
+  </section>
+
+    <!-- Videos -->
+
+    <section class="container-fluid py-7">
+
+        <div class="row wide">
+
+            <div class="col-lg-8 mb-4 mb-lg-0">
+
+                <div class="narrow">
+
+                    <dl class="row justify-content-between py-2 mb-0">
+                        <dt class="col-md">
+                            <strong class="d-block">Peter Gottesman</strong>
+                            Founder &amp; President
+                        </dt>
+                        <dt class="col-md-auto">
+                            <a href="mailto:peterg@esrevenge.org">peterg@esrevenge.org</a>
+                        </dt>
+                    </dl>
+
+                    <dl class="row justify-content-between border-top py-2 mb-0">
+                        <dt class="col-md">
+                            <strong class="d-block">Media &amp; PR Inquiries</strong>
+                            Gregory Crowley
+                        </dt>
+                        <dt class="col-md-auto">
+                            <a href="mailto:greg@esrevenge.org">greg@esrevenge.org</a>
+                        </dt>
+                    </dl>
+
+                    <dl class="row justify-content-between border-top py-2 mb-0">
+                        <dt class="col-md">
+                            <strong class="d-block">Conservation Project Proposals</strong>
+                        </dt>
+                        <dt class="col-md-auto">
+                            <a href="mailto:projects@esrevenge.org">projects@esrevenge.org</a>
+                        </dt>
+                    </dl>
+
+                    <dl class="row justify-content-between border-top py-2 mb-0">
+                        <dt class="col-md">
+                            <strong class="d-block">Scientific Inquiries</strong>
+                        </dt>
+                        <dt class="col-md-auto">
+                            <a href="mailto:science@esrevenge.org">science@esrevenge.org</a>
+                        </dt>
+                    </dl>
+
+                    <dl class="row justify-content-between border-top py-2 mb-0">
+                        <dt class="col-md">
+                            <strong class="d-block">To Advertise on ESR</strong>
+                        </dt>
+                        <dt class="col-md-auto">
+                            <a href="mailto:advertising@esrevenge.org">advertising@esrevenge.org</a>
+                        </dt>
+                    </dl>
+
+                    <dl class="row justify-content-between border-top py-2 mb-0">
+                        <dt class="col-md">
+                            <strong class="d-block">General Information</strong>
+                        </dt>
+                        <dt class="col-md-auto">
+                            <a href="mailto:info@esrevenge.org">info@esrevenge.org</a>
+                        </dt>
+                    </dl>
+
+                </div>
+                <!-- .narrow -->
+
+            </div>
+            <!-- .col -->
+
+            <div class="col-lg-4">
+                <address class="narrow">
+                    <strong>Endangered Species Revenge</strong><br>
+                    45 Rockefeller Plaza<br>
+                    20th Floor<br>
+                    New York, NY 10111
+                </address>
+                <p>+1-914-725-5602</p>
+            </div>
+            <!-- .col -->
+
+        </div>
+        <!-- .row -->
+
+      
+    </section>
+    <!-- .container-fluid -->
+    
+</main>
+<!-- #content -->
+    
+<?php get_footer(); ?>
