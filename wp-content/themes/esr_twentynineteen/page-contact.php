82d743ab52eeaf2e23ec77d2e838e18adbda2e3b
Carl Avidano <cavidano@gmail.com>
2018-12-10 04:53:11 -0500

    updated contact page

--- wp-content/themes/esr_twentynineteen/page-contact.php
+++ wp-content/themes/esr_twentynineteen/page-contact.php
@@ -21,88 +21,80 @@ get_header(); ?>
 
     <section class="container-fluid py-7">
 
-        <div class="row wide">
+        <div class="narrow mb-5">
 
-            <div class="col-lg-8 mb-4 mb-lg-0">
+            <dl class="row justify-content-between py-2 mb-0">
+                <dt class="col-md">
+                    <strong class="d-block">Peter Gottesman</strong>
+                    Founder &amp; President
+                </dt>
+                <dt class="col-md-auto">
+                    <a href="mailto:peterg@esrevenge.org">peterg@esrevenge.org</a>
+                </dt>
+            </dl>
 
-                <div class="narrow">
+            <dl class="row justify-content-between border-top py-2 mb-0">
+                <dt class="col-md">
+                    <strong class="d-block">Media &amp; PR Inquiries</strong>
+                    Gregory Crowley
+                </dt>
+                <dt class="col-md-auto">
+                    <a href="mailto:greg@esrevenge.org">greg@esrevenge.org</a>
+                </dt>
+            </dl>
 
-                    <dl class="row justify-content-between py-2 mb-0">
-                        <dt class="col-md">
-                            <strong class="d-block">Peter Gottesman</strong>
-                            Founder &amp; President
-                        </dt>
-                        <dt class="col-md-auto">
-                            <a href="mailto:peterg@esrevenge.org">peterg@esrevenge.org</a>
-                        </dt>
-                    </dl>
+            <dl class="row justify-content-between border-top py-2 mb-0">
+                <dt class="col-md">
+                    <strong class="d-block">Conservation Project Proposals</strong>
+                </dt>
+                <dt class="col-md-auto">
+                    <a href="mailto:projects@esrevenge.org">projects@esrevenge.org</a>
+                </dt>
+            </dl>
 
-                    <dl class="row justify-content-between border-top py-2 mb-0">
-                        <dt class="col-md">
-                            <strong class="d-block">Media &amp; PR Inquiries</strong>
-                            Gregory Crowley
-                        </dt>
-                        <dt class="col-md-auto">
-                            <a href="mailto:greg@esrevenge.org">greg@esrevenge.org</a>
-                        </dt>
-                    </dl>
+            <dl class="row justify-content-between border-top py-2 mb-0">
+                <dt class="col-md">
+                    <strong class="d-block">Scientific Inquiries</strong>
+                </dt>
+                <dt class="col-md-auto">
+                    <a href="mailto:science@esrevenge.org">science@esrevenge.org</a>
+                </dt>
+            </dl>
 
-                    <dl class="row justify-content-between border-top py-2 mb-0">
-                        <dt class="col-md">
-                            <strong class="d-block">Conservation Project Proposals</strong>
-                        </dt>
-                        <dt class="col-md-auto">
-                            <a href="mailto:projects@esrevenge.org">projects@esrevenge.org</a>
-                        </dt>
-                    </dl>
+            <dl class="row justify-content-between border-top py-2 mb-0">
+                <dt class="col-md">
+                    <strong class="d-block">To Advertise on ESR</strong>
+                </dt>
+                <dt class="col-md-auto">
+                    <a href="mailto:advertising@esrevenge.org">advertising@esrevenge.org</a>
+                </dt>
+            </dl>
 
-                    <dl class="row justify-content-between border-top py-2 mb-0">
-                        <dt class="col-md">
-                            <strong class="d-block">Scientific Inquiries</strong>
-                        </dt>
-                        <dt class="col-md-auto">
-                            <a href="mailto:science@esrevenge.org">science@esrevenge.org</a>
-                        </dt>
-                    </dl>
-
-                    <dl class="row justify-content-between border-top py-2 mb-0">
-                        <dt class="col-md">
-                            <strong class="d-block">To Advertise on ESR</strong>
-                        </dt>
-                        <dt class="col-md-auto">
-                            <a href="mailto:advertising@esrevenge.org">advertising@esrevenge.org</a>
-                        </dt>
-                    </dl>
-
-                    <dl class="row justify-content-between border-top py-2 mb-0">
-                        <dt class="col-md">
-                            <strong class="d-block">General Information</strong>
-                        </dt>
-                        <dt class="col-md-auto">
-                            <a href="mailto:info@esrevenge.org">info@esrevenge.org</a>
-                        </dt>
-                    </dl>
-
-                </div>
-                <!-- .narrow -->
-
-            </div>
-            <!-- .col -->
-
-            <div class="col-lg-4">
-                <address class="narrow">
-                    <strong>Endangered Species Revenge</strong><br>
-                    45 Rockefeller Plaza<br>
-                    20th Floor<br>
-                    New York, NY 10111
-                </address>
-                <p>+1-914-725-5602</p>
-            </div>
-            <!-- .col -->
+            <dl class="row justify-content-between border-top py-2 mb-0">
+                <dt class="col-md">
+                    <strong class="d-block">General Information</strong>
+                </dt>
+                <dt class="col-md-auto">
+                    <a href="mailto:info@esrevenge.org">info@esrevenge.org</a>
+                </dt>
+            </dl>
 
         </div>
-        <!-- .row -->
+        <!-- .narrow -->
 
+        <div class="narrow mb-5">
+
+            <address class="narrow">
+                <strong>Endangered Species Revenge</strong><br>
+                45 Rockefeller Plaza<br>
+                20th Floor<br>
+                New York, NY 10111
+            </address>
+
+            <p>+1-914-725-5602</p>
+
+        </div>
+        <!-- .narrow -->
       
     </section>
     <!-- .container-fluid -->
